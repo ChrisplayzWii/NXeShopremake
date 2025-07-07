cdd48624b96c491a8d0749f5c6d21f5be9395e79
TheWiiArchivist! <191655784+ChrisplayzWii@users.noreply.github.com>
2025-07-07 13:54:55 -0700

    better ver fr fr

--- js/NX.js
+++ js/NX.js
@@ -1,3 +1,12 @@
-var sidenav = document.getElementById("sidebar");
+var sidenav = document.getelementById("sidebar");
 sidenav.focus();
 
+function hovernav(navoption) {
+    navoption.style.backgroundColor = "#ffffff41";
+    navoption.style.color = "#ffe4e3";
+}
+
+function leavenav(navoption) {
+    navoption.style.backgroundColor = "";
+    navoption.style.color = "";
+}
