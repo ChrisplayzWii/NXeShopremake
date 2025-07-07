a7406c26b124d3bf70fd13655a3e9f4fff8c8043
TheWiiArchivist! <191655784+ChrisplayzWii@users.noreply.github.com>
2025-07-07 13:17:17 -0700

    -iq fr how did i mess that up

--- js/NX.js
+++ js/NX.js
@@ -3,54 +3,54 @@ sidenav.focus();
 
 function hovernavoption(number) {
    if (number == 1) {
-    document.getElementById("featured-navopt").classList.add="selectednav";
+    document.getElementById("featured-navopt").classList.add("selectednav");
    }
       if (number == 2) {
-    document.getElementById("recentreleases-navopt").classList.add="selectednav";
+    document.getElementById("recentreleases-navopt").classList.add("selectednav");
    }
          if (number == 3) {
-    document.getElementById("recentreleases-navopt").classList.add="selectednav";
+    document.getElementById("recentreleases-navopt").classList.add("selectednav");
    }
          if (number == 4) {
-    document.getElementById("greatdeals-navopt").classList.add="selectednav";
+    document.getElementById("greatdeals-navopt").classList.add("selectednav");
    }
          if (number == 5) {
-    document.getElementById("bestsellers-navopt").classList.add="selectednav";
+    document.getElementById("bestsellers-navopt").classList.add("selectednav");
    }
          if (number == 6) {
-    document.getElementById("comingsoon-navopt").classList.add="selectednav";
+    document.getElementById("comingsoon-navopt").classList.add("selectednav");
    }
          if (number == 7) {
-    document.getElementById("nso-navopt").classList.add="selectednav";
+    document.getElementById("nso-navopt").classList.add("selectednav");
    }
          if (number == 8) {
-    document.getElementById("entercode-navopt").classList.add="selectednav";
+    document.getElementById("entercode-navopt").classList.add("selectednav");
    }
 }
 
 function unhovernavoption(number) {
    if (number == 1) {
-    document.getElementById("featured-navopt").classList.remove="selectednav";
+    document.getElementById("featured-navopt").classList.remove("selectednav");
    }
       if (number == 2) {
-    document.getElementById("recentreleases-navopt").classList.remove="selectednav";
+    document.getElementById("recentreleases-navopt").classList.remove("selectednav");
    }
          if (number == 3) {
-    document.getElementById("recentreleases-navopt").classList.remove="selectednav";
+    document.getElementById("recentreleases-navopt").classList.remove("selectednav");
    }
          if (number == 4) {
-    document.getElementById("greatdeals-navopt").classList.remove="selectednav";
+    document.getElementById("greatdeals-navopt").classList.remove("selectednav");
    }
          if (number == 5) {
-    document.getElementById("bestsellers-navopt").classList.remove="selectednav";
+    document.getElementById("bestsellers-navopt").classList.remove("selectednav");
    }
          if (number == 6) {
-    document.getElementById("comingsoon-navopt").classList.remove="selectednav";
+    document.getElementById("comingsoon-navopt").classList.remove("selectednav");
    }
          if (number == 7) {
-    document.getElementById("nso-navopt").classList.remove="selectednav";
+    document.getElementById("nso-navopt").classList.remove("selectednav");
    }
          if (number == 8) {
-    document.getElementById("entercode-navopt").classList.remove="selectednav";
+    document.getElementById("entercode-navopt").classList.remove("selectednav");
    }
 }
