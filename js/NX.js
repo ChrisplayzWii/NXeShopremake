d714d702fe94a9467860f84de620fff59ea3e331
TheWiiArchivist! <191655784+ChrisplayzWii@users.noreply.github.com>
2025-07-08 11:42:31 -0700

    beeg update (titles too but so many img)

--- js/NX.js
+++ js/NX.js
@@ -1,14 +1,170 @@
-// sidenav
-var sidenav = document.getelementById("sidebar");
-sidenav.focus();
+var loadedin = 0;
 
-function hovernav(navoption) {
-    navoption.style.backgroundColor = "#ffffff41";
-    navoption.style.color = "#ffe4e3";
+document.addEventListener("DOMContentLoaded", (event) => {
+if (loadedin = 0) {
+  alert("welcome to NX eShop!");
+}
+});
+
+function fullyloadedin() {
+loadedin = 1;
+initialloadintoeshop();
+
+var content = document.getElementById('content'); 
+content.onclick = function() {
+  movenavformaincontent();
+};
+setTimeout(clearfeaturedfocus,100);
 }
 
-function leavenav(navoption) {
-    navoption.style.backgroundColor = "inherit";
-    navoption.style.color = "inherit";
+function clearfeaturedfocus() {var loadcheckk = document.getElementById('maincontent'); loadcheckk.onmouseover = function() {};var featuree = document.getElementById('featured-navopt'); featuree.blur();}
+
+function initialloadintoeshop() {
+  // first page they see
+  changecontentfornav('2');
+      var ele = document.getElementById("featured-navopt");
+    ele.focus();
 }
-// sidenav
+
+
+
+function movebacknav() {
+document.getElementById("sidebar").style.width="480px";
+document.getElementById("sidebar").style.overflow="hidden";
+document.getElementById("sidebarcoverwhenmoved").style.display="none";
+changetitlesizeback();
+}
+
+
+function movenavformaincontent() {
+
+document.getElementById("sidebar").style.width="50px";
+document.getElementById("sidebar").style.overflow="hidden";
+document.getElementById("sidebarcoverwhenmoved").style.display="block";
+changetitlesize();
+}
+
+function changetitlesize() {
+const titlelist = document.getElementById('titleslist');
+
+if (titlelist) {
+  titlelist.style.transformOrigin = 'top left';
+  titlelist.style.transform = 'scale(1.2)';
+}
+
+document.getElementById("sectiontitle").style.fontSize="28px";
+}
+
+function changetitlesizeback() {
+const titlelist = document.getElementById('titleslist');
+
+if (titlelist) {
+  titlelist.style.transformOrigin = 'top left';
+  titlelist.style.transform = 'scale(1)';
+}
+
+document.getElementById("sectiontitle").style.fontSize="23px";
+}
+
+
+function changecontentfornav(cont) {
+
+ if (cont ==  1) {
+   var titleofcatalog = document.getElementById("sectiontitle"); var catalog = document.getElementById("titleslist");
+    if (titleofcatalog) { titleofcatalog.innerText="Search/Browse"; }
+/*
+    if (catalog) {
+     catalog.innerHTML=
+      `
+
+      `;
+     }
+*/
+ }
+  if (cont ==  2) {
+ var titleofcatalog = document.getElementById("sectiontitle"); var catalog = document.getElementById("titleslist");
+    if (titleofcatalog) { titleofcatalog.innerText="Featured"; }
+/*
+    if (catalog) {
+     catalog.innerHTML=
+      `
+
+      `;
+     }
+*/
+ }
+  if (cont ==  3) {
+    var titleofcatalog = document.getElementById("sectiontitle"); var catalog = document.getElementById("titleslist");
+    if (titleofcatalog) { titleofcatalog.innerText="Recent Releases"; }
+/*
+    if (catalog) {
+     catalog.innerHTML=
+      `
+
+      `;
+     }
+*/
+ }
+  if (cont ==  4) {
+    var titleofcatalog = document.getElementById("sectiontitle"); var catalog = document.getElementById("titleslist");
+    if (titleofcatalog) { titleofcatalog.innerText="Great Deals";} 
+/*
+    if (catalog) {
+     catalog.innerHTML=
+      `
+
+      `;
+     }
+*/
+ }
+  if (cont ==  5) {
+    var titleofcatalog = document.getElementById("sectiontitle"); var catalog = document.getElementById("titleslist");
+    if (titleofcatalog) { titleofcatalog.innerText="Best Sellers"; }
+/*
+    if (catalog) {
+     catalog.innerHTML=
+      `
+
+      `;
+     }
+*/
+  }
+  if (cont ==  6) {
+    var titleofcatalog = document.getElementById("sectiontitle"); var catalog = document.getElementById("titleslist");
+    if (titleofcatalog) { titleofcatalog.innerText="Coming Soon"; }
+/*
+    if (catalog) {
+     catalog.innerHTML=
+      `
+
+      `;
+     }
+*/
+ }
+  if (cont ==  7) {
+    var titleofcatalog = document.getElementById("sectiontitle"); var catalog = document.getElementById("titleslist");
+    if (titleofcatalog) { titleofcatalog.innerText="Nintendo Switch Online"; }
+/*
+    if (catalog) {
+     catalog.innerHTML=
+      `
+
+      `;
+     }
+*/
+ }
+  if (cont ==  8) {
+    var titleofcatalog = document.getElementById("sectiontitle"); var catalog = document.getElementById("titleslist");
+    if (titleofcatalog) { titleofcatalog.innerText="Redeem Code"; }
+/*
+    if (catalog) {
+     catalog.innerHTML=
+      `
+
+      `;
+     }
+*/
+ }
+
+}
+
