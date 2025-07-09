fa50e7df91c8323eb39dd988ca946fac9de588ef
TheWiiArchivist! <191655784+ChrisplayzWii@users.noreply.github.com>
2025-07-09 14:57:36 -0700

    grammer issue + credit in wrong place lol

--- js/NX.js
+++ js/NX.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 /* footer */
-    document.addEventListener("scroll", function () {
+ /* https://stackoverflow.com/a/37020542 */
+document.addEventListener("scroll", function () {
       document.getElementById("footer").style.height="0px";
 
       setTimeout(function(){document.getElementById("footer").style.height="69px";},350);
 });
 
-if (navigator.userAgent.toLowerCase().includes("WifiWebAuthApplet")) {
+if (navigator.userAgent.toLowerCase().includes("wifiwebauthapplet")) {
   /* it has its own :p */
   document.getElementById("footer").style.display = "none";  
 } else {
   document.getElementById("footer").style.display = "block";  
-      /* https://stackoverflow.com/a/37020542 */
 
 }
 /* footer */
