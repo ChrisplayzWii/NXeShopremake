5fe0c983747b892a2cd0843468633fbcf1f6f2a4
TheWiiArchivist! <191655784+ChrisplayzWii@users.noreply.github.com>
2025-07-09 19:54:10 -0700

    update

--- js/NX.js
+++ js/NX.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 /* footer */
- /* https://stackoverflow.com/a/37020542 */
-document.addEventListener("scroll", function () {
-      document.getElementById("footer").style.height="0px";
 
-      setTimeout(function(){document.getElementById("footer").style.height="69px";},350);
+ /* https://stackoverflow.com/a/37020542 */
+document.addEventListener("scroll", function () 
+{
+      document.getElementById("footer").style.height="0px";
 });
+
 
 if (navigator.userAgent.toLowerCase().includes("wifiwebauthapplet")) {
   /* it has its own :p */
@@ -19,3 +20,51 @@ if (navigator.userAgent.toLowerCase().includes("wifiwebauthapplet")) {
 
 
 })
+
+
+
+function closenav() {
+  changetitlesize();
+  document.getElementById("tempcover").style.display="none";
+   document.getElementById("sidenav").style.transition=".2s ease-in-out";
+   document.getElementById("contentaftersidenav").style.transition=".2s ease-in-out";
+
+    document.getElementById("sidenav").style.width="60px"; document.getElementById("sidenav").style.maxWidth="60px"; document.getElementById("sidenavlinks").style.display="none"; document.getElementById("sidenavcover").style.display="block";
+    document.getElementById("contentaftersidenav").style.marginLeft="60px"; document.getElementById("contentaftersidenav").style.width="1280px"; document.getElementById("contentaftersidenav").style.maxWidth="1220px";
+    document.getElementById("innerContent").style.maxWidth="1165px"; 
+}
+
+function opennav() {
+  var bgofside = document.getElementById("sidenav").style.backgroundColor;
+  document.getElementById("tempcover").style.backgroundColor= bgofside;
+  document.getElementById("tempcover").style.display="block";
+  changetitlesizeback();
+  document.getElementById("sidenav").style.transition=".5s ease-in-out";
+  document.getElementById("contentaftersidenav").style.transition=".5s ease-in-out";
+      document.getElementById("sidenavcover").style.display="none";
+    document.getElementById("sidenav").style.width="410px"; document.getElementById("sidenav").style.maxWidth="410px";
+    document.getElementById("contentaftersidenav").style.marginLeft="410px"; document.getElementById("contentaftersidenav").style.width="870px"; document.getElementById("contentaftersidenav").style.maxWidth="870px";
+    document.getElementById("innerContent").style.maxWidth="815px";
+    setTimeout(showlinksforgoodanim,100); function showlinksforgoodanim() {document.getElementById("sidenavlinks").style.display="block"; document.getElementById("tempcover").style.display="none";}
+}
+
+
+function changetitlesize() {
+const titlelist = document.getElementById('titlelist');
+
+if (titlelist) {
+  titlelist.style.transformOrigin = 'top left';
+  titlelist.style.transform = 'scale(1.2)';
+}
+
+}
+
+function changetitlesizeback() {
+const titlelist = document.getElementById('titlelist');
+
+if (titlelist) {
+  titlelist.style.transformOrigin = 'top left';
+  titlelist.style.transform = 'scale(1)';
+}
+
+}
